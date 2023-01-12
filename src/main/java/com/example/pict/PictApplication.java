6fea4bd1690e7463cbc852451fec96b7cf2c0954
motoki <kounotori100588@i.softbank.jp>
2023-01-12 22:56:28 +0900

    first commit

---
+++ src/main/java/com/example/pict/PictApplication.java
@@ -0,0 +1,13 @@
+package com.example.pict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pict/PictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
